dd19caf94041042bc76cea8e043030ffc98b629e
laurenspriem <github_uni.fishers@aleeas.com>
2024-06-21 12:50:10 +0530

    git init

---
+++ android/app/src/main/kotlin/com/example/flutter_onnx_inference_crash/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_onnx_inference_crash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter ONNX crash',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter ONNX crash'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  bool _isRunning = false;
+  bool _shouldPause = false;
+
+  void _runInfiniteIndexing() {
+    while (true) {
+      setState(() {
+        _counter++;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Inference runs of both models:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _runInfiniteIndexing,
+        tooltip: 'Start running inference',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
